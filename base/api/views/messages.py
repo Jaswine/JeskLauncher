66ed0a6c99443be912087c7c5aa3aca1e8d31f80
Jaswine <jaswine883@gmail.com>
2023-09-08 09:59:43 +0600

    change data sorting

--- base/api/views/messages.py
+++ base/api/views/messages.py
@@ -14,16 +14,8 @@ def messages_list(request):
     google_todos_messages = []
     google_gmail_messages = []
     google_youtube_messages = []
-
-    filter = []
     # socialApp = SocialApp.objects.get(provider='google')
     
-    filter_get = request.GET.get('filter')
-    
-    if filter_get:
-        filter = filter_get.split(',') 
-        print(filter)   
-            
     socialGoogleToken = SocialToken.objects.filter(account__user=request.user, account__provider='google').last()
     if socialGoogleToken:
         access_token = socialGoogleToken.token
@@ -31,20 +23,16 @@ def messages_list(request):
         print(filter)
 
         # # CALLENDAR GOOGLE        
-        if 'Google_Event' in filter:   
-            google_calendar_messages = google_calendar.CallendarService(email_list, access_token, included_apps)
+        google_calendar_messages = google_calendar.CallendarService(email_list, access_token, included_apps)
 
         # # GOOGLE TASKS        
-        if 'Google_Todo' in filter:
-            google_todos_messages = google_todos.GoogleTodoService(email_list, access_token, included_apps)
+        google_todos_messages = google_todos.GoogleTodoService(email_list, access_token, included_apps)
 
         # # GOOGLE GMAIL
-        if 'Gmail' in filter:
-            google_gmail_messages = google_gmail.GoogleGmailService(email_list, access_token, get_email_text, get_header_value, included_apps)
+        google_gmail_messages = google_gmail.GoogleGmailService(email_list, access_token, get_email_text, get_header_value, included_apps)
         
         # # GOOGLE YOUTUBE
-        if 'YouTube' in filter:
-            google_youtube_messages = google_youtube.GoogleYoutubeService(email_list, access_token, included_apps)
+        google_youtube_messages = google_youtube.GoogleYoutubeService(email_list, access_token, included_apps)
         
         if len(email_list) > 0:
             data = [{
@@ -66,27 +54,23 @@ def messages_list(request):
             data = sorted(data, key=lambda x: x['created_time'])
             
             # print(f'\n\n{google_calendar_messages}\n\n')
-            if google_calendar_messages: 
-                google_calendar_messages = sorted(google_calendar_messages, key=lambda x: x['created_time'])
+            google_calendar_messages = sorted(google_calendar_messages, key=lambda x: x['created_time'])
             
-            if google_gmail_messages:
-                google_gmail_messages = sorted(google_gmail_messages, key=lambda x: x['created_time'])
+            google_gmail_messages = sorted(google_gmail_messages, key=lambda x: x['created_time'])
             
-            if google_todos_messages:
-                google_todos_messages = sorted(google_todos_messages, key=lambda x: x['created_time'])
+            google_todos_messages = sorted(google_todos_messages, key=lambda x: x['created_time'])
             
-            if google_youtube_messages:
-                google_youtube_messages = sorted(google_youtube_messages, key=lambda x: x['created_time'])
+            google_youtube_messages = sorted(google_youtube_messages, key=lambda x: x['created_time'])
             
             return JsonResponse({
                 'status':'success',
                 'included_apps': included_apps,
                 'all_messages': data[::-1],
                 'services': {
-                    'Google_Event': google_calendar_messages,
-                    'Gmail':  google_gmail_messages,
-                    'Google_Todo': google_todos_messages,
-                    'YouTube': google_youtube_messages,
+                    'Google_Event': google_calendar_messages[::-1],
+                    'Gmail':  google_gmail_messages[::-1],
+                    'Google_Todo': google_todos_messages[::-1],
+                    'YouTube': google_youtube_messages[::-1],
                 },
             }, safe=False)
             
