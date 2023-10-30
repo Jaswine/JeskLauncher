b41ccbe2814e8288386284e707f32d2792352132
Jaswine <vaselesstolarov813@gmail.com>
2023-10-30 15:20:50 +0600

    added error pages

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,6 +3,11 @@ from django.urls import path, include
 
 from . import views
 
+handler401 = 'mysite.views.tr_handler401'
+handler403 = 'mysite.views.tr_handler403'
+handler404 = 'mysite.views.tr_handler404'
+handler500 = 'mysite.views.tr_handler500'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
@@ -11,7 +16,6 @@ urlpatterns = [
     path('privacy-police', views.privacy_policy, name='privacy'),
     path('terms-of-use', views.terms_of_use, name='terms'),
     path('delete-instructions', views.delete_instructions, name='delete-instructions'),
-    path('404', views.error_404, name='404'),
     
     path('accounts/', include('allauth.urls')),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -16,8 +16,30 @@ def delete_instructions(request):
         'title': 'Delete Instructions'
     })
 
-# Error pages
-def error_404(request):
-    return render(request, 'errors/404Page.html', {
-        'title': 'Delete Instructions'
-    })
+# 404 error
+def tr_handler404(request, exception):
+    return render(request, 'errors/error_page.html', {
+        'title': 'Error 404',
+        'text': 'Page not found'
+    }, status=404)
+
+# 403 error
+def tr_handler403(request, exception):
+    return render(request, 'errors/error_page.html', {
+        'title': 'Error 403',
+        'text': 'You do not have the necessary permissions to access the requested resource'
+    }, status=403)
+
+# 401 error
+def tr_handler401(request, exception):
+    return render(request, 'errors/error_page.html', {
+        'title': 'Error 401',
+        'text': "You don't authorized to access the requested resource"
+    }, status=401)
+
+# 500 error
+def tr_handler500(request):
+    return render(request, 'errors/error_page.html', {
+        'title': 'Error 500',
+        'text': "..."
+    }, status=500)
